3686a2bef873b276329faadaf3ab2457b3a3b8a8
Tshepo Kau <tshepokau@Tshepos-MacBook-Pro.local>
2020-04-15 15:33:02 +0200

    finished section 1.5

---
+++ ch1/fetch/fetch.go
@@ -0,0 +1,26 @@
+//Fetch does an http request and prints the response body
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	prefix := "http://"
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, prefix) {
+			url = prefix + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Fprintf(os.Stdout, "Status code: %v\n", resp.StatusCode)
+		io.Copy(os.Stdout, resp.Body)
+	}
+}
